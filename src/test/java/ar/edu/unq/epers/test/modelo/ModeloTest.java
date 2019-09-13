49e3154a3baf1c60260cae45a8c07a95e9613379
lattanzio <taato.lat@gmail.com>
2019-09-13 18:37:30 -0300

    Se agrega mockito al pomp y el file ModeloTest

---
+++ src/test/java/ar/edu/unq/epers/test/modelo/ModeloTest.java
@@ -0,0 +1,19 @@
+package ar.edu.unq.epers.test.modelo;
+
+import ar.edu.unq.epers.bichomon.backend.model.especie.Especie;
+import ar.edu.unq.epers.bichomon.backend.model.especie.TipoBicho;
+import ar.edu.unq.epers.bichomon.backend.service.data.DataService;
+import ar.edu.unq.epers.bichomon.backend.service.data.DataServiceImp;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class ModeloTest {
+
+    @Before
+
+}
